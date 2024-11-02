2054b5e8a2df8734704e7628696467d5e3b3c85b
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-11-02 15:40:13 +0100

    Change keybindings (alpha, hop, mini)

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     require('hop').setup({ keys = 'etovxqpdygfblzhckisuran' })
 
-    vim.keymap.set({ 'n', 'x', 'o' }, 'gl', vim.cmd.HopLine, { desc = "Jump to line (Hop)" })
-    vim.keymap.set({ 'n', 'x', 'o' }, 's', vim.cmd.HopChar1, { desc = "Jump to char1 (Hop)" })
+    -- vim.keymap.set({ 'n', 'x', 'o' }, 'gl', vim.cmd.HopLine, { desc = "Jump to line (Hop)" })
+    -- vim.keymap.set({ 'n', 'x', 'o' }, 's', vim.cmd.HopChar1, { desc = "Jump to char1 (Hop)" })
     vim.keymap.set('n', 'gw', vim.cmd.HopWord, { desc = "Jump to word (Hop)" })
   end
 }
