253bfb0391fe9623d48f1dc30171af8537a35c64
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-11-16 00:49:26 +0100

    Clean plugins
    Remove:
    - hardtime
    - harpoon
    - precognition
    - vim-be-good
    
    Improve:
    - text-objs

--- lua/plugins/text-objs.lua
+++ lua/plugins/text-objs.lua
@@ -13,25 +13,7 @@ return {
     })
 
     vim.keymap.set({ 'o', 'x' }, 'ie', '<cmd>lua require("various-textobjs").entireBuffer()<CR>')
-
-    -- vim.keymap.set({ 'o', 'x' }, 'ay', '<cmd>lua require("various-textobjs").key("around")<CR>')
-    -- vim.keymap.set({ 'o', 'x' }, 'iy', '<cmd>lua require("various-textobjs").key("inner")<CR>')
-    --
-    -- vim.keymap.set({ 'o', 'x' }, 'iv', '<cmd>lua require("various-textobjs").value("inner")<CR>')
-    -- vim.keymap.set({ 'o', 'x' }, 'av', '<cmd>lua require("various-textobjs").value("around")<CR>')
-
     vim.keymap.set({ 'o', 'x' }, 'il', '<cmd>lua require("various-textobjs").lineCharacterwise("inner")<CR>')
-    vim.keymap.set({ 'o', 'x' }, 'al', '<cmd>lua require("various-textobjs").lineCharacterwise("around")<CR>')
 
-    vim.keymap.set(
-      { 'o', 'x' },
-      'ix',
-      '<cmd>lua require("various-textobjs").htmlAttribute("inner")<CR>'
-    )
-    vim.keymap.set(
-      { 'o', 'x' },
-      'ax',
-      '<cmd>lua require("various-textobjs").htmlAttribute("around")<CR>'
-    )
   end
 }
