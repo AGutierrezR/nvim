309c5a32f62d1a7d19af49d758db263b53328211
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-11-25 01:12:14 +0100

    Improve keymaps from fzf-lua and GitSigns

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -23,10 +23,18 @@ return {
       }
     })
 
+    vim.keymap.set('n', '<leader><leader>', require('fzf-lua').resume, { desc = 'FZF Resume' })
+
     vim.keymap.set('n', '<leader>f', require('fzf-lua').files, { desc = 'Search File' })
     vim.keymap.set('n', '<leader>/', require('fzf-lua').live_grep, { desc = 'Search by Grep' })
+    vim.keymap.set('v', '<leader>/', require('fzf-lua').grep_visual, { desc = 'Find Selection' })
+    vim.keymap.set('n', '<leader>7', require('fzf-lua').grep_cword, { desc = 'Search Word (under cursor)' })
     vim.keymap.set('n', '<leader>b', require('fzf-lua').buffers, { desc = 'Find existing buffers' })
-    vim.keymap.set('n', '<leader>R', require('fzf-lua').oldfiles, { desc = 'Recently used files' })
+    vim.keymap.set('n', '<leader>R', require('fzf-lua').registers, { desc = 'Registers' })
     vim.keymap.set('n', '<leader>?', require('fzf-lua').commands, { desc = 'Find Commands' })
+
+    -- Git related keymaps 
+    vim.keymap.set('n', '<leader>gc', require('fzf-lua').git_bcommits, { desc = 'Browse File Commits' })
+    vim.keymap.set('n', '<leader>gs', require('fzf-lua').git_status, { desc = 'Git Status' })
   end
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -16,6 +16,6 @@ return {
 
     vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', { silent = true })
     vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', { silent = true })
-    vim.keymap.set('n', '<leader>gl', '<cmd>silent !tmux neww lazygit<CR>')
+    vim.keymap.set('n', '<leader>gr', ':Gitsigns reset_hunk<CR>')
   end
 }
