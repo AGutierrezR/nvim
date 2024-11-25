a684d5cc7dffa15a828a4d8999eadb32a486fd58
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-11-25 01:10:39 +0100

    Improve lualine displaying

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,71 @@
+local mode_map = {
+	["NORMAL"] = "N",
+	["O-PENDING"] = "N?",
+	["INSERT"] = "I",
+	["VISUAL"] = "V",
+	["V-BLOCK"] = "VB",
+	["V-LINE"] = "VL",
+	["V-REPLACE"] = "VR",
+	["REPLACE"] = "R",
+	["COMMAND"] = "!",
+	["SHELL"] = "SH",
+	["TERMINAL"] = "T",
+	["EX"] = "X",
+	["S-BLOCK"] = "SB",
+	["S-LINE"] = "SL",
+	["SELECT"] = "S",
+	["CONFIRM"] = "Y?",
+	["MORE"] = "M",
+}
+
+local function show_macro_recording()
+	local recording_register = vim.fn.reg_recording()
+	if recording_register == "" then
+		return ""
+	else
+		return "󰑋 " .. recording_register
+	end
+end
+
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    'yavorski/lualine-macro-recording.nvim'
-  },
-  opts = {
-    sections = {
-      lualine_c = { 'filename', 'macro_recording' },
-      lualine_x = { 'encoding', 'filetype' },
-    }
-  },
-  config = function(_, opts)
-    require('lualine').setup(opts)
-  end
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		sections = {
+			lualine_a = {
+				{
+					"mode",
+					fmt = function(s)
+						return mode_map[s] or s
+					end,
+				},
+			},
+			lualine_b = {
+				{ "branch", icon = "󰘬" },
+				"diff",
+				"diagnostics",
+			},
+			lualine_c = {
+				{
+					"filename",
+					symbols = {
+						modified = "󰐖", -- Text to show when the file is modified.
+						readonly = "", -- Text to show when the file is non-modifiable or readonly.
+					},
+				},
+				{
+					show_macro_recording,
+					color = { fg = "#333333", bg = "#ff6666" },
+					separator = { left = "", right = "" },
+				},
+			},
+			lualine_x = { "encoding", "filetype" },
+			lualine_y = { nil }
+		},
+	},
+	config = function(_, opts)
+		require("lualine").setup(opts)
+	end,
 }
