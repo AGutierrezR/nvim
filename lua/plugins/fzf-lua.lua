5d9691f44ed5f5e09b56558644661ea477f67dbe
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-12-31 19:20:01 +0100

    Format + add keymaps to fzf-lua and GitSigns

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,57 +1,70 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  --   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    require("fzf-lua").setup({
-      defaults = {
-        git_icons = false,
-        file_icons = false,
-        color_icons = false,
-        formatter = "path.filename_first",
-      },
-      files = {
-        cwd_prompt = false
-      },
-      lsp = {
-        symbols = {
-          prompt = "❯ "
-        }
-      },
-      marks = {
-        marks = "[a-zA-Z]"
-      },
-      git = {
-        files = {
-          prompt = "❯ "
-        },
-        status = {
-          prompt = "❯ "
-        }
-      },
-    })
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	--   dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- calling `setup` is optional for customization
+		require("fzf-lua").setup({
+			defaults = {
+				git_icons = false,
+				file_icons = false,
+				color_icons = false,
+				formatter = "path.filename_first",
+			},
+			keymap = {
+				fzf = {
+					["ctrl-q"] = "select-all+accept",
+				},
+			},
+			oldfiles = {
+				cwd_only = true,
+				include_current_session = true,
+			},
+			files = {
+				cwd_prompt = false,
+			},
+			lsp = {
+				symbols = {
+					prompt = "❯ ",
+				},
+			},
+			marks = {
+				marks = "[a-zA-Z]",
+			},
+			git = {
+				files = {
+					prompt = "❯ ",
+				},
+				status = {
+					prompt = "❯ ",
+				},
+			},
+		})
+		-- use `fzf-lua` for replace vim.ui.select
+		require("fzf-lua").register_ui_select()
 
-    vim.keymap.set('n', '<leader><leader>', require('fzf-lua').resume, { desc = 'FZF Resume' })
+		vim.keymap.set("n", "<leader>'", require("fzf-lua").resume, { desc = "FZF Resume" })
 
-    vim.keymap.set('n', '<leader>f', require('fzf-lua').files, { desc = 'Search File' })
-    vim.keymap.set('n', '<leader>/', require('fzf-lua').live_grep, { desc = 'Search by Grep' })
-    vim.keymap.set('v', '<leader>/', require('fzf-lua').grep_visual, { desc = 'Find Selection' })
-    vim.keymap.set('n', '<leader>7', require('fzf-lua').grep_cword, { desc = 'Search Word (under cursor)' })
-    vim.keymap.set('n', '<leader>b', require('fzf-lua').buffers, { desc = 'Find existing buffers' })
-    vim.keymap.set('n', '<leader>R', require('fzf-lua').registers, { desc = 'Registers' })
-    vim.keymap.set('n', '<leader>?', require('fzf-lua').commands, { desc = 'Find Commands' })
-    vim.keymap.set('n', '<leader>m', require('fzf-lua').marks, { desc = 'Marks' })
+		vim.keymap.set("n", "<leader>f", require("fzf-lua").files, { desc = "Search File" })
+		vim.keymap.set("n", "<leader>F", require("fzf-lua").oldfiles, { desc = "Search File" })
+		vim.keymap.set("n", "<leader>/", require("fzf-lua").live_grep, { desc = "Search by Grep" })
+		vim.keymap.set("v", "<leader>/", require("fzf-lua").grep_visual, { desc = "Find Selection" })
+		vim.keymap.set("n", "<leader>7", require("fzf-lua").grep_cword, { desc = "Search Word (under cursor)" })
+		vim.keymap.set("n", "<leader>b", require("fzf-lua").buffers, { desc = "Find existing buffers" })
+		vim.keymap.set("n", "<leader>R", require("fzf-lua").registers, { desc = "Registers" })
+		vim.keymap.set("n", "<leader>?", require("fzf-lua").commands, { desc = "Find Commands" })
+		vim.keymap.set("n", "<leader>z", require("fzf-lua").lgrep_curbuf, { desc = "Find on current buffer" })
+		vim.keymap.set("n", "<leader>m", require("fzf-lua").marks, { desc = "Marks" })
 
-    -- Git related keymaps 
-    vim.keymap.set('n', '<leader>gb', require('fzf-lua').git_branches, { desc = 'Branches' })
-    vim.keymap.set('n', '<leader>gc', require('fzf-lua').git_bcommits, { desc = 'Browse File Commits' })
-    vim.keymap.set('n', '<leader>gC', require('fzf-lua').git_commits, { desc = 'Browse Commits' })
-    vim.keymap.set('n', '<leader>gs', require('fzf-lua').git_status, { desc = 'Git Status' })
-    vim.keymap.set('n', '<leader>gf', require('fzf-lua').git_files, { desc = 'Git Files' })
-    vim.keymap.set('n', '<leader>gS', require('fzf-lua').git_stash, { desc = 'Git Stash' })
+		-- Git related keymaps
+		vim.keymap.set("n", "<leader>gb", require("fzf-lua").git_branches, { desc = "Branches" })
+		vim.keymap.set("n", "<leader>gc", require("fzf-lua").git_bcommits, { desc = "Browse File Commits" })
+		vim.keymap.set("n", "<leader>gC", require("fzf-lua").git_commits, { desc = "Browse Commits" })
+		vim.keymap.set("n", "<leader>gs", require("fzf-lua").git_status, { desc = "Git Status" })
+		vim.keymap.set("n", "<leader>gf", require("fzf-lua").git_files, { desc = "Git Files" })
+		vim.keymap.set("n", "<leader>gS", require("fzf-lua").git_stash, { desc = "Git Stash" })
 
-    -- Vim features
-    vim.keymap.set('n', 'z=', require('fzf-lua').spell_suggest, { desc = 'Spelling suggestions' })
-  end
+		-- Vim features
+		vim.keymap.set("n", "z=", require("fzf-lua").spell_suggest, { desc = "Spelling suggestions" })
+	end,
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -17,12 +17,14 @@ return {
     vim.keymap.set('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', { desc = 'Preview hunk', silent = true })
     vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', { desc = 'Toggle line blame', silent = true })
     vim.keymap.set('n', '<leader>gd', ':Gitsigns reset_hunk<CR>', { desc = 'Discard hunk', silent = true })
+    vim.keymap.set('v', '<leader>gd', ':Gitsigns reset_hunk<CR>', { desc = 'Discard hunk', silent = true })
     vim.keymap.set('n', '<leader>gD', ':Gitsigns reset_buffer<CR>', { desc = 'Discard buffer changes', silent = true })
     vim.keymap.set('n', '<leader>gk', ':Gitsigns prev_hunk<CR>', { desc = 'Prev hunk', silent = true })
     vim.keymap.set('n', '<leader>gj', ':Gitsigns next_hunk<CR>', { desc = 'Next hunk', silent = true })
 
     vim.keymap.set('n', '<leader>ga', ':Gitsigns stage_buffer<CR>', { desc = 'Stage Buffer', silent = true })
     vim.keymap.set('n', '<leader>g+', ':Gitsigns stage_hunk<CR>', { desc = 'Stage hunk', silent = true })
+    vim.keymap.set('v', '<leader>g+', ':Gitsigns stage_hunk<CR>', { desc = 'Stage hunk', silent = true })
     vim.keymap.set('n', '<leader>g-', ':Gitsigns undo_stage_hunk<CR>', { desc = 'Unstage hunk', silent = true })
   end
 }
