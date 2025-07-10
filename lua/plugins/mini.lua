fcf52573140e7f92953953ca7d330a807e411402
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-07-10 23:35:07 +0200

    Prevent mini pair to be trigger on typr files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -127,6 +127,10 @@ return {
     version = false,
     config = function()
       require("mini.pairs").setup()
+
+      -- Disable pairs for `typr` filetype
+      local f = function (args) vim.b[args.buf].minipairs_disable = true end
+      vim.api.nvim_create_autocmd('Filetype', { pattern = 'typr', callback = f })
     end,
   },
   {
