2ea7bd625bb1e99988e689c06553641964d5fd2a
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-08-01 16:21:52 +0200

    Configure return to last edit position when opening files

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -50,6 +50,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
