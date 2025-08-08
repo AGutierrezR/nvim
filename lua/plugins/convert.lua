ff759cc7baf307616cf3f5d72e1de61c56e351f5
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-08-08 13:19:13 +0200

    Remove unnecessary plugins

--- lua/plugins/convert.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"jsongerber/nvim-px-to-rem",
-  event = { 'BufReadPre', 'BufNewFile' },
-	config = true,
-	--If you need to set some options replace the line above with:
-	-- config = function()
-	--     require('nvim-px-to-rem').setup()
-	-- end,
-}
