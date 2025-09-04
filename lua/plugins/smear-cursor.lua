09cf5973674021ca8aac84efcd54d35371e8b286
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-09-05 00:11:18 +0200

    feat(smear-cursor): add smear-cursor

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    stiffness = 0.5,
+    smear_between_neighbor_lines = false,
+    trailing_stiffness = 0.5,
+    never_draw_over_target = false,
+  },
+}
