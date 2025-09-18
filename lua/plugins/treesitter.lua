565d4b05245a68efcbe4358459a84c998667b489
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-09-18 22:27:02 +0200

    feat(treesitter): add opt property

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,116 +2,121 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     event = { "LazyFile", "VeryLazy" },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "lua",
-          "vim",
-          "javascript",
-          "typescript",
-          "json",
-          "html",
-          "markdown",
-          "markdown_inline",
-          "css",
-          "scss",
-          "yaml",
-          "toml",
-          "go",
-        },
+    cmd = { "TSUpdate", "TSInstall", "TSLog", "TSUninstall" },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "lua",
+        "vim",
+        "javascript",
+        "typescript",
+        "json",
+        "jsonc",
+        "html",
+        "markdown",
+        "markdown_inline",
+        "css",
+        "scss",
+        "yaml",
+        "toml",
+        "go",
+      },
 
-        highlight = {
+      highlight = { enable = true, },
+      incremental_selection = {
+        enable = false,
+        keymaps = {
+          node_incremental = "<Enter>",
+          node_decremental = "<Tab>",
+          scope_incremental = false,
+        },
+      },
+
+      textobjects = {
+        select = {
           enable = true,
-        },
-
-        incremental_selection = {
-          enable = false,
+          lookahead = true,
+          include_surrounding_whitespace = false,
           keymaps = {
-            node_incremental = "<Enter>",
-            node_decremental = "<Tab>",
-            scope_incremental = false,
+            -- ["af"] = "@function.outer", -- Use mini.ai instead
+            -- ["if"] = "@function.inner", -- Use mini.ai instead
+            -- ["ac"] = "@class.outer",	-- Use mini.ai instead
+            -- ["ic"] = "@class.inner",	-- Use mini.ai instead
+            -- ["is"] = "@assignment.inner",
+            -- ["as"] = "@assignment.outer",
+            -- ["ig"] = "@block.inner", -- Use mini.ai instead
+            -- ["ag"] = "@block.outer", -- Use mini.ai instead
+            -- ["ia"] = "@parameter.inner", -- Use mini.ai instead
+            -- ["aa"] = "@parameter.outer", -- Use mini.ai instead
+            -- ["ik"] = "@call.inner",
+            -- ["ak"] = "@call.outer",
+            -- ["i/"] = "@comment.inner", -- Not working in Javascript
+            -- ["a/"] = "@comment.outer",
+            -- ["ir"] = "@conditional.inner", -- Use mini.ai instead
+            -- ["ar"] = "@conditional.outer", -- Use mini.ai instead
+            -- ["io"] = "@loop.inner", -- Use mini.ai instead
+            -- ["ao"] = "@loop.outer", -- Use mini.ai instead
+            -- ["it"] = "@return.inner", -- Not working in Javascript
+            -- ["at"] = "@return.outer", -- Not working in Javascript
           },
         },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]f"] = "@function.outer",
+            ["]="] = "@assignment.outer",
+            ["]c"] = "@class.outer",
+            ["]g"] = "@block.outer",
+            ["]a"] = "@parameter.outer",
+            ["]k"] = "@call.outer",
+            ["]/"] = "@comment.outer",
+            ["]r"] = "@conditional.outer",
+            ["]o"] = "@loop.outer",
+            -- ["]t"] = "@return.outer",
+          },
+          goto_next_end = {
+            ["]F"] = "@function.outer",
+            ["]C"] = "@class.outer",
+            ["]G"] = "@block.outer",
+            ["]A"] = "@parameter.outer",
+            ["]K"] = "@call.outer",
+            ["]?"] = "@comment.outer",
+            ["]R"] = "@conditional.outer",
+            ["]O"] = "@loop.outer",
+            -- ["]T"] = "@return.outer",
+          },
+          goto_previous_start = {
+            ["[f"] = "@function.outer",
+            ["[="] = "@assignment.outer",
+            ["[c"] = "@class.outer",
+            ["[g"] = "@block.outer",
+            ["[a"] = "@parameter.outer",
+            ["[k"] = "@call.outer",
+            ["[/"] = "@comment.outer",
+            ["[r"] = "@conditional.outer",
+            ["[o"] = "@loop.outer",
+            -- ["[t"] = "@return.outer",
+          },
+          goto_previous_end = {
+            ["[F"] = "@function.outer",
+            ["[C"] = "@class.outer",
+            ["[G"] = "@block.outer",
+            ["[A"] = "@parameter.outer",
+            ["[K"] = "@call.outer",
+            ["[?"] = "@comment.outer",
+            ["[R"] = "@conditional.outer",
+            ["[O"] = "@loop.outer",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local TS = require("nvim-treesitter.configs")
 
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            include_surrounding_whitespace = false,
-            keymaps = {
-              -- ["af"] = "@function.outer", -- Use mini.ai instead
-              -- ["if"] = "@function.inner", -- Use mini.ai instead
-              -- ["ac"] = "@class.outer",	-- Use mini.ai instead
-              -- ["ic"] = "@class.inner",	-- Use mini.ai instead
-              -- ["is"] = "@assignment.inner",
-              -- ["as"] = "@assignment.outer",
-              -- ["ig"] = "@block.inner", -- Use mini.ai instead
-              -- ["ag"] = "@block.outer", -- Use mini.ai instead
-              -- ["ia"] = "@parameter.inner", -- Use mini.ai instead
-              -- ["aa"] = "@parameter.outer", -- Use mini.ai instead
-              -- ["ik"] = "@call.inner",
-              -- ["ak"] = "@call.outer",
-              -- ["i/"] = "@comment.inner", -- Not working in Javascript
-              -- ["a/"] = "@comment.outer",
-              -- ["ir"] = "@conditional.inner", -- Use mini.ai instead
-              -- ["ar"] = "@conditional.outer", -- Use mini.ai instead
-              -- ["io"] = "@loop.inner", -- Use mini.ai instead
-              -- ["ao"] = "@loop.outer", -- Use mini.ai instead
-              -- ["it"] = "@return.inner", -- Not working in Javascript
-              -- ["at"] = "@return.outer", -- Not working in Javascript
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]="] = "@assignment.outer",
-              ["]c"] = "@class.outer",
-              ["]g"] = "@block.outer",
-              ["]a"] = "@parameter.outer",
-              ["]k"] = "@call.outer",
-              ["]/"] = "@comment.outer",
-              ["]r"] = "@conditional.outer",
-              ["]o"] = "@loop.outer",
-              -- ["]t"] = "@return.outer",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]C"] = "@class.outer",
-              ["]G"] = "@block.outer",
-              ["]A"] = "@parameter.outer",
-              ["]K"] = "@call.outer",
-              ["]?"] = "@comment.outer",
-              ["]R"] = "@conditional.outer",
-              ["]O"] = "@loop.outer",
-              -- ["]T"] = "@return.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[="] = "@assignment.outer",
-              ["[c"] = "@class.outer",
-              ["[g"] = "@block.outer",
-              ["[a"] = "@parameter.outer",
-              ["[k"] = "@call.outer",
-              ["[/"] = "@comment.outer",
-              ["[r"] = "@conditional.outer",
-              ["[o"] = "@loop.outer",
-              -- ["[t"] = "@return.outer",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[C"] = "@class.outer",
-              ["[G"] = "@block.outer",
-              ["[A"] = "@parameter.outer",
-              ["[K"] = "@call.outer",
-              ["[?"] = "@comment.outer",
-              ["[R"] = "@conditional.outer",
-              ["[O"] = "@loop.outer",
-            },
-          },
-        },
-      })
+      -- setup treesitter
+      TS.setup(opts)
+
       -- Configuration to treat JSONC files as JSON
       vim.treesitter.language.register("json", "jsonc")
 
