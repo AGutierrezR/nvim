3420cf8d346f51c2d146611bab9b62aec189b681
Andres Gutierrez <agutierrez.er@gmail.com>
2025-10-03 19:23:27 +0200

    feat(smear-cursor): delete smear-cursor

--- lua/plugins/smear-cursor.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sphamba/smear-cursor.nvim",
-  opts = {
-    stiffness = 0.5,
-    smear_between_neighbor_lines = false,
-    trailing_stiffness = 0.5,
-    never_draw_over_target = false,
-  },
-}
