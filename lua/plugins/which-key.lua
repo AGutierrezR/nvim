e3832890d1f9b260b4846afdf51558684b605a22
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-10-17 18:03:25 +0200

    feat(which-key): format file

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,50 +1,50 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts_extend = { "spec" },
-	opts = {
-		preset = "helix",
-		delay = 300,
-		spec = {
-			{ "<leader>g", group = "+Git" },
-			{ "[`", desc = "Prev mark" },
-			{ "]`", desc = "Next mark" },
-			{ "<leader>a", group = "+AI" },
-			{ "<leader>c", group = "+Code" },
-			{ "<leader>s", group = "+Search" },
-			{ "<leader>f", group = "+Files/Find" },
-			{ "<leader>b", group = "+Buffers" },
-			{ "<leader>x", group = "+diagnostics/quickfix" },
-			{ "<leader>q", group = "+quit/session" },
-		},
-		win = {
-			height = {
-				max = math.huge,
-			},
-		},
-		plugins = {
-			spelling = {
-				enabled = false,
-			},
-		},
-		icons = {
-			rules = false,
-			breadcrumb = "",
-			separator = "󱦰",
-			group = "󰹍 ",
-		},
-	},
-	keys = {
-		{
-			"<leader>gg",
-			function()
-				require("which-key").show({ keys = '<leader>g', loop = true })
-			end,
-			desc = "Git (Hydra Mode)",
-		},
-	},
-	config = function(_, opts)
-		local wk = require("which-key")
-		wk.setup(opts)
-	end,
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    preset = "helix",
+    delay = 300,
+    spec = {
+      { "<leader>g", group = "+Git" },
+      { "[`", desc = "Prev mark" },
+      { "]`", desc = "Next mark" },
+      { "<leader>a", group = "+AI" },
+      { "<leader>c", group = "+Code" },
+      { "<leader>s", group = "+Search" },
+      { "<leader>f", group = "+Files/Find" },
+      { "<leader>b", group = "+Buffers" },
+      { "<leader>x", group = "+diagnostics/quickfix" },
+      { "<leader>q", group = "+quit/session" },
+    },
+    win = {
+      height = {
+        max = math.huge,
+      },
+    },
+    plugins = {
+      spelling = {
+        enabled = false,
+      },
+    },
+    icons = {
+      rules = false,
+      breadcrumb = "",
+      separator = "󱦰",
+      group = "󰹍 ",
+    },
+  },
+  keys = {
+    {
+      "<leader>gg",
+      function()
+        require("which-key").show({ keys = "<leader>g", loop = true })
+      end,
+      desc = "Git (Hydra Mode)",
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+  end,
 }
