c743a45370a4e481885e85b61b4f1b6eced1e1dd
Andres Gutierrez <agutierrez.codes@gmail.com>
2025-11-07 23:09:56 +0100

    feat(fugitive): add fugitive and diffview

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,7 @@
+return {
+  { "tpope/vim-fugitive", cmd = { "G", "Git", "GBrowse", "Gdiffsplit", "Gvdiffsplit" } },
+  {
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+}
