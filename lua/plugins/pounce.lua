dc05cccdeeeaba026f497a606c4ac71528c0d20c
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-01-23 16:27:41 +0100

    feat(pounce): improve pounce repeat keymap

--- lua/plugins/pounce.lua
+++ lua/plugins/pounce.lua
@@ -1,16 +1,12 @@
 -- Pounce is a motion plugin similar to EasyMotion, Sneak, Hop, and Lightspeed. It's based on incremental fuzzy search
 return {
-  "rlane/pounce.nvim",
-  event = "LazyFile",
-  opts = {},
-  keys = {
-    { "s", "<cmd>Pounce<CR>", mode = { "n", "x", "o" }, desc = "Pounce" },
-    { "<leader>sp", "<cmd>PounceRepeat<CR>", mode = { "n", "x", "o" }, desc = "Pounce Repeat" },
+  {
+    "rlane/pounce.nvim",
+    event = "LazyFile",
+    opts = {},
+    keys = {
+      { "s", "<cmd>Pounce<CR>", mode = { "n", "x", "o" }, desc = "Pounce" },
+      { "<leader>;", "<cmd>PounceRepeat<CR>", mode = { "n", "x", "o" }, desc = "Pounce Repeat" },
+    },
   },
-  -- config = function()
-  --   require("pounce").setup()
-  --
-  --   vim.keymap.set({ "n", "x", "o" }, "s", vim.cmd.Pounce, { desc = "Pounce" })
-  --   vim.keymap.set({ "n", "x", "o" }, "<leader>sp", vim.cmd.PounceRepeat, { desc = "Pounce Repeat" })
-  -- end,
 }
