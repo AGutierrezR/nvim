020a66506473079070620966f55db1cd8368324b
Andres Gutierrez <agutierrez.codes@gmail.com>
2026-01-30 00:36:17 +0100

    feat(mini-operators): add keymaps modes

--- lua/plugins/mini-operators.lua
+++ lua/plugins/mini-operators.lua
@@ -3,18 +3,18 @@ return {
   "echasnovski/mini.operators",
   version = false,
   keys = {
-    { "cx", desc = "Exchange text" },
-    { "cm", desc = "Multiply text" },
-    { "cr", desc = "Replace text with register" },
-    { "cs", desc = "Sort text" },
+    { "cx", desc = "Exchange text", mode = { "n", "x" } },
+    { "cm", desc = "Multiply text", mode = { "n", "x" } },
+    { "cr", desc = "Replace text with register", mode = { "n", "x" } },
+    { "cs", desc = "Sort text", mode = { "n", "x" } },
   },
   opts = {
     evaluate = {
       prefix = "",
     },
 
-    -- Exchange text regions
     exchange = {
+      -- Exchange text regions
       prefix = "cx",
 
       -- Whether to reindent new text to match previous indent
