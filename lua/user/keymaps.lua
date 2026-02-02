2167ae18939e781b5d20175e35f16a8feb6cf583
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-02-02 10:08:45 +0100

    feat(keymaps): categorize keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,23 +1,104 @@
 local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
 
--- keymap("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+-- ════════════════════════════════════════════════════════════════════════════
+-- Essential Operations
+-- ════════════════════════════════════════════════════════════════════════════
 
--- Close terminal
-keymap("t", "<esc><esc>", "<c-\\><c-n>")
+-- Save file
+keymap("n", "<C-S>", "<Cmd>silent! update | redraw<CR>", { desc = "Save" })
+keymap({ "i", "x" }, "<C-S>", "<Esc><Cmd>silent! update | redraw<CR>", { desc = "Save and go to Normal mode" })
+
+-- Quit
+keymap("n", "<leader>qq", "<cmd>q<cr>", { desc = "Quit" })
+keymap("n", "<leader>qQ", "<cmd>qa<cr>", { desc = "Quit All" })
+
+-- Clear search highlighting
+keymap("n", "<Esc>", ":nohlsearch<cr>", { silent = true, desc = "Clear search highlighting" })
+
+-- remove Q keymap
+keymap("n", "Q", "<nop>")
+
+-- ════════════════════════════════════════════════════════════════════════════
+-- Window Navigation (no prefix for speed)
+-- ════════════════════════════════════════════════════════════════════════════
+
+-- Windows resizing
+keymap("n", "<C-w>.", ":vertical resize +20<cr>")
+keymap("n", "<C-w>,", ":vertical resize -20<cr>")
+
+-- ════════════════════════════════════════════════════════════════════════════
+-- Line Movement (Visual Mode)
+-- ════════════════════════════════════════════════════════════════════════════
+
+keymap("v", "J", ":m .+1<CR>==", { desc = "Move line down in v mode" })
+keymap("v", "K", ":m .-2<CR>==", { desc = "Move line down in v mode" })
+keymap("x", "J", ":move '>+1<CR>gv-gv", { desc = "Move line down in x mode" })
+keymap("x", "K", ":move '<-2<CR>gv-gv", { desc = "Move line down in x mode" })
+
+-- ════════════════════════════════════════════════════════════════════════════
+-- Better Navigation
+-- ════════════════════════════════════════════════════════════════════════════
+
+-- Wrapped line navigation
+keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, desc = "Up (wrapped)" })
+keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, desc = "Down (wrapped)" })
 
 keymap("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down and center cursor" })
 keymap("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up and center cursor" })
 
+-- ════════════════════════════════════════════════════════════════════════════
+-- Better Editing
+-- ════════════════════════════════════════════════════════════════════════════
+
+-- delete single character without copying into register
+keymap("n", "x", '"_x')
+
 -- Select last pasted text
 keymap("n", "gV", "`[v`]", { desc = "Select last pasted text" })
 
--- windows
-keymap("n", "<C-w>.", ":vertical resize +20<cr>")
-keymap("n", "<C-w>,", ":vertical resize -20<cr>")
+-- better indent handling
+keymap("v", "<", "<gv", { desc = "Indent Left" })
+keymap("v", ">", ">gv", { desc = "Indent Right" })
 
--- remove Q keymap
-keymap("n", "Q", "<nop>")
+-- Yank block
+keymap("n", "YY", "va{Vy", { desc = "Yank Block {}" })
+
+-- Better join lines
+keymap("n", "J", "mzJ`z")
+
+-- Split line (opposite of J)
+keymap(
+  "n",
+  "X",
+  ":keeppatterns substitute/\\s*\\%#\\s*/\\r/e <bar> normal! ==^<cr>",
+  { desc = "Split Line", silent = true }
+)
+
+-- Search within visual selection
+-- Use <C-\\><C-n> to exit visual mode properly and not <Esc> which can have issues in some terminals
+keymap("x", "g/", "<C-\\><C-n>`</\\%V", { silent = false, desc = "Search inside visual selection" })
+
+-- Yank to system clipboard
+keymap({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+keymap({ "n", "v" }, "gy", '"+y', { desc = "Copy to system clipboard" })
+
+-- Move yank register 0 to system clipboard
+keymap({ "n" }, "yc", function()
+  local target_register = "+"
+  -- Get the content and type of the yank register
+  local yank_content = vim.fn.getreg("0")
+  local yank_type = vim.fn.getregtype("0")
+  -- Move the content to the specified register
+  vim.fn.setreg(target_register, yank_content, yank_type)
+  print(string.format("Text moved to register '%s'.", target_register))
+end, { desc = "Paste register 0 to system clipboard" })
+
+-- copy to to system clipboard (till end of line)
+keymap({ "n" }, "gY", '"+y$', { desc = "Yank to clipboard EOL" })
+
+-- paste from the system clipboard
+keymap({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+keymap({ "n", "v" }, "gp", '"+p', { desc = "Paste from system clipboard" })
 
 -- Helper function to insert empty lines
 _G.put_empty_line = function(put_above)
@@ -28,7 +109,7 @@ _G.put_empty_line = function(put_above)
   end
 
   local target = vim.fn.line(".") - (vim.g._put_empty_line_above and 1 or 0)
-  vim.fn.append( target, vim.fn["repeat"]({ "" }, vim.v.count1))
+  vim.fn.append(target, vim.fn["repeat"]({ "" }, vim.v.count1))
 end
 
 -- NOTE: if you don't want to support dot-repeat, use this snippet:
@@ -36,16 +117,17 @@ end
 -- keymap('n', 'gO', "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>")
 -- keymap('n', 'go', "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>")
 -- ```
-keymap( "n", "gO", "v:lua.put_empty_line(v:true)", { expr = true, desc = "Put empty line above" })
-keymap( "n", "go", "v:lua.put_empty_line(v:false)", { expr = true, desc = "Put empty line below" })
+keymap("n", "gO", "v:lua.put_empty_line(v:true)", { expr = true, desc = "Put empty line above" })
+keymap("n", "go", "v:lua.put_empty_line(v:false)", { expr = true, desc = "Put empty line below" })
 
+-- ════════════════════════════════════════════════════════════════════════════
+-- File related Operations
+-- ════════════════════════════════════════════════════════════════════════════
 
-keymap("n", "<C-S>", "<Cmd>silent! update | redraw<CR>", { desc = "Save" })
-keymap({ "i", "x" }, "<C-S>", "<Esc><Cmd>silent! update | redraw<CR>", { desc = "Save and go to Normal mode" })
-keymap("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit All", silent = true })
-
--- file related keymaps
+-- Create new file
 keymap("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File", silent = true })
+
+-- Copy file paths with line and column numbers
 keymap("n", "<leader>fyp", function()
   local file = vim.fn.expand("%:.") -- Relative path
   local line = vim.fn.line(".") -- Current line
@@ -57,62 +139,20 @@ keymap("n", "<leader>fyp", function()
   vim.notify("filepath: " .. path_with_pos .. " has been copied to clipboard", vim.log.levels.INFO)
 end, { desc = "Copy relative filepath with line and column", silent = true })
 
+-- Copy different file paths
 keymap("n", "<leader>fya", ":let @+ = expand('%:p')<cr>", { desc = "Copy absolute filepath", silent = true })
 keymap("n", "<leader>fyr", ":let @+ = expand('%:.')<cr>", { desc = "Copy relative filepath", silent = true })
 keymap("n", "<leader>fyn", ":let @+ = expand('%:t')<cr>", { desc = "Copy filename", silent = true })
 
--- clear search highlighting
-keymap("n", "<Esc>", ":nohlsearch<cr>", { silent = true, desc = "Clear search highlighting" })
+-- ════════════════════════════════════════════════════════════════════════════
+-- Buffer and Tab Management
+-- ════════════════════════════════════════════════════════════════════════════
 
--- search in file what is selected in visual mode
--- keymap("v", "*", '"6y/\\V<C-r>6<CR>N', { desc = "Search forward in file what is selected in visual mode" })
--- keymap("v", "#", '"6y?\\V<C-r>6<CR>N', { desc = "Search backward in file what is selected in visual mode" })
-
--- Better join lines
-keymap("n", "J", "mzJ`z")
-
--- yank to system clipboard
-keymap({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
-keymap({ "n", "v" }, "gy", '"+y', { desc = "Copy to system clipboard" })
-keymap({ "n" }, "yc", function()
-  local target_register = "+"
-  -- Get the content and type of the yank register
-  local yank_content = vim.fn.getreg("0")
-  local yank_type = vim.fn.getregtype("0")
-  -- Move the content to the specified register
-  vim.fn.setreg(target_register, yank_content, yank_type)
-  print(string.format("Text moved to register '%s'.", target_register))
-end, { desc = "Paste register 0 to system clipboard" })
--- copy to to system clipboard (till end of line)
-keymap({ "n" }, 'gY', '"+y$', { desc = 'Yank to clipboard EOL' })
-
--- paste from the system clipboard
-keymap({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
-keymap({ "n", "v" }, "gp", '"+p', { desc = "Paste from system clipboard" })
-
--- better indent handling
-keymap("v", "<", "<gv")
-keymap("v", ">", ">gv")
-
--- Move lines in visual mode
-keymap("v", "J", ":m .+1<CR>==", { desc = "Move line down in v mode" })
-keymap("v", "K", ":m .-2<CR>==", { desc = "Move line down in v mode" })
-keymap("x", "J", ":move '>+1<CR>gv-gv", { desc = "Move line down in x mode" })
-keymap("x", "K", ":move '<-2<CR>gv-gv", { desc = "Move line down in x mode" })
-
--- delete single character without copying into register
-keymap("n", "x", '"_x')
-
--- -- delete the whole file content
--- keymap("n", "die", [[<Cmd>:%d<CR>]])
-
--- Buffer navigation
 -- Switch to last accessed buffer
 keymap("n", "ga", "<cmd>b#<CR>", { desc = "Switch to last accessed buffer" })
-keymap("n", "<leader>bw", ":w<CR>", { desc = "Save buffer", silent = true })
+
+-- Close current buffer
 keymap("n", "<leader>bD", "<cmd>:bd<CR>", { desc = "Delete Buffer and Window", silent = true })
-keymap("n", "<leader>bn", "<cmd>:bnext<CR>", { desc = "Next buffer" })
-keymap("n", "<leader>bp", "<cmd>:bprevious<CR>", { desc = "Prev buffer" })
 
 -- Tabs
 keymap("n", "<C-t>", ":tabnew<cr>", { desc = "New tab" }) -- remove ctags keymap
@@ -124,7 +164,9 @@ keymap("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 keymap("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 keymap("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
--- keymap("n", "<C-k>", "k")
+-- ════════════════════════════════════════════════════════════════════════════
+-- TMUX
+-- ════════════════════════════════════════════════════════════════════════════
 
 keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
@@ -132,41 +174,9 @@ keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 keymap("n", "[q", "<cmd>cprev<CR>", { desc = "Go to prev quickfix item" })
 keymap("n", "]q", "<cmd>cnext<CR>", { desc = "Go to next quickfix item" })
 
--- Search within visual selection
--- Use <C-\\><C-n> to exit visual mode properly and not <Esc> which can have issues in some terminals
-keymap("x", "g/", "<C-\\><C-n>`</\\%V", { silent = false, desc = "Search inside visual selection" })
+-- ════════════════════════════════════════════════════════════════════════════
+-- Terminal
+-- ════════════════════════════════════════════════════════════════════════════
 
-keymap("n", "<leader>xq", function()
-  local qf_list = vim.fn.getqflist()
-  for _, win in ipairs(vim.fn.getwininfo()) do
-    if win.quickfix == 1 then
-      vim.cmd("cclose")
-      return
-    end
-  end
-  if #qf_list > 0 then
-    vim.cmd("copen")
-  else
-    print("Quickfix list is empty")
-  end
-end, { desc = "Toggle Quickfix list" })
-
--- Add current location to QF list
-keymap("n", "<leader>xa", function()
-  vim.fn.setqflist({
-    {
-      filename = vim.fn.expand("%:p"),
-      lnum = vim.fn.line("."),
-      col = vim.fn.col("."),
-      text = vim.fn.getline("."),
-    },
-  }, "a")
-  print("Current position added to QF list.")
-end, { desc = "Add current location to QF list" })
-
--- Clean QF list
-keymap("n", "<leader>xc", function()
-  vim.fn.setqflist({}, "r")
-
-  print("Quickfix list cleared.")
-end, { desc = "Clean QF list" })
+-- Close terminal
+keymap("t", "<esc><esc>", "<c-\\><c-n>")
