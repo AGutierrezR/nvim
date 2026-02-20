3228df0756547bdbbd38192523b3098e3b09225c
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-02-20 18:14:22 +0100

    feat(codecompanion): add more prompts

--- lua/config/codecompanion/prompts.lua
+++ lua/config/codecompanion/prompts.lua
@@ -1,19 +1,67 @@
+local system = require("config.codecompanion.system")
 local M = {}
 
 M.PROMPT_LIBRARY = {
   ["Better Naming"] = {
-    strategy = "chat",
-    description = "Give betting naming for the provided code snippet.",
+    strategy = "inline",
+    description = "Give better naming for the provided code snippet  -- inline",
     opts = {
-      modes = { "v" }, -- Only available in visual mode
-      short_name = "better-naming",
+      modes = { "v" },
+      short_name = "naming",
       auto_submit = true,
-      is_slash_cmd = true,
+      user_prompt = false,
+      stop_context_insertion = true,
     },
     prompts = {
       {
         role = "user",
-        content = "Please provide better names for the following variables and functions.",
+        content = function(context)
+          local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+          return "Please provide better names for the following variables and functions:\n\n```"
+            .. context.filetype
+            .. "\n"
+            .. code
+            .. "\n```\n\n"
+        end,
+        opts = {
+          contains_code = true,
+        },
+      },
+    },
+  },
+  ["Refactor"] = {
+    strategy = "inline",
+    description = "Refactor the provided code snippet  -- inline, has_system_prompt",
+    opts = {
+      modes = { "v" },
+      short_name = "refactor",
+      auto_submit = true,
+      user_prompt = false,
+      stop_context_insertion = true,
+    },
+    prompts = {
+      {
+        role = "system",
+        content = system.CODE_REFACTOR,
+        opts = {
+          visible = false,
+        },
+      },
+      {
+        role = "user",
+        content = function(context)
+          local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
+
+          return "Please refactor the following code to improve its clarity and readability:\n\n```"
+            .. context.filetype
+            .. "\n"
+            .. code
+            .. "\n```\n\n"
+        end,
+        opts = {
+          contains_code = true,
+        },
       },
     },
   },
@@ -33,7 +81,7 @@ M.PROMPT_LIBRARY = {
         content = function(context)
           local code = require("codecompanion.helpers.actions").get_code(context.start_line, context.end_line)
 
-          return "Please provide jsdocs code for the following code and suggest to have better naming to improve readability.\n\n```"
+          return "Please provide jsdocs code for the following code.\n\n```"
             .. context.filetype
             .. "\n"
             .. code
