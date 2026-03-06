d5c90e9ca89fceff3a6fd48f3a01785d0c965e4f
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-03-06 22:00:52 +0100

    feat(sidekick,copilot): improve configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,16 +4,23 @@ return {
   event = "InsertEnter",
   build = ":Copilot auth",
   opts = {
+    server_opts_overrides = {
+      settings = {
+        telemetry = {
+          telemetryLevel = "off",
+        },
+      },
+    },
     suggestion = {
-      enabled = true,
-      auto_trigger = true,
+      -- enabled = false,
+      auto_trigger = false,
       keymap = {
-        accept = "<Tab>",
+        accept = false,
         accept_word = false,
         accept_line = false,
-        next = "<M-]>",
-        prev = "<M-[>",
-        dismiss = "<C-]>",
+        next = "<C-g>", -- '<M-]>'
+        prev = "<C-G>", -- '<M-[>'
+        dismiss = "<C-e>", -- '<Esc>'
       },
     },
     panel = { enabled = false },
@@ -23,5 +30,20 @@ return {
       yaml = true,
     },
     copilot_node_command = vim.env.HOME .. "/Library/Caches/fnm_multishells/976_1768269005343/bin/node", -- Node.js version must be > 20
+  },
+  keys = {
+    {
+      "<Tab>",
+      function()
+        if require("copilot.suggestion").is_visible() then
+          require("copilot.suggestion").accept()
+          return
+        end
+        return "<Tab>" -- Fallback to normal tab.
+      end,
+      mode = { "i" },
+      desc = "Copilot: Accept",
+      expr = true,
+    },
   },
 }
--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -32,18 +32,18 @@ return {
     },
   },
   keys = {
-    {
-      "<tab>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if not require("sidekick").nes_jump_or_apply() then
-          print("No more edit suggestions")
-          return "<Tab>" -- fallback to normal tab
-        end
-      end,
-      expr = true,
-      desc = "Goto/Apply Next Edit Suggestion",
-    },
+    -- {
+    --   "<tab>",
+    --   function()
+    --     -- if there is a next edit, jump to it, otherwise apply it if any
+    --     if not require("sidekick").nes_jump_or_apply() then
+    --       print("No more edit suggestions")
+    --       return "<Tab>" -- fallback to normal tab
+    --     end
+    --   end,
+    --   expr = true,
+    --   desc = "Goto/Apply Next Edit Suggestion",
+    -- },
     {
       "<leader>oo",
       function()
