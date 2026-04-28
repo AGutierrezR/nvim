07d76f796e2bceab566b53c509820ae4903fcefa
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2026-04-28 23:55:03 +0200

    feat(lsp-config): format file

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -177,14 +177,14 @@ return {
         },
       }
 
-			-- Ensure the servers and tools above are installed
-			--
-			-- To check the current status of installed tools and/or manually install
-			-- other tools, you can run
-			--    :Mason
-			--
-			-- You can press `g?` for help in this menu.
-			local ensure_installed = vim.tbl_keys(servers or {})
+      -- Ensure the servers and tools above are installed
+      --
+      -- To check the current status of installed tools and/or manually install
+      -- other tools, you can run
+      --    :Mason
+      --
+      -- You can press `g?` for help in this menu.
+      local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         "stylua", -- Used to format Lua code
         "eslint",
