1cd8dd549c9d15294996385873879f821d0d27a3
Andres Gutierrez Ramirez <a.gutierrez.ramirez.contractor@bbva.com>
2026-05-06 22:54:35 +0200

    feat(tabout.nvim): add tabout nvim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -15,12 +15,12 @@ return {
       -- enabled = false,
       auto_trigger = false,
       keymap = {
-        accept = false,
+        accept = "<M-Tab>",
         accept_word = false,
         accept_line = false,
         next = "<C-g>", -- '<M-]>'
         prev = "<C-G>", -- '<M-[>'
-        dismiss = "<C-e>", -- '<Esc>'
+        dismiss = "<C-e>", -- '<Esc>' 
       },
     },
     panel = { enabled = false },
@@ -30,20 +30,5 @@ return {
       yaml = true,
     },
     copilot_node_command = vim.env.HOME .. "/Library/Caches/fnm_multishells/976_1768269005343/bin/node", -- Node.js version must be > 20
-  },
-  keys = {
-    {
-      "<Tab>",
-      function()
-        if require("copilot.suggestion").is_visible() then
-          require("copilot.suggestion").accept()
-          return
-        end
-        return "<Tab>" -- Fallback to normal tab.
-      end,
-      mode = { "i" },
-      desc = "Copilot: Accept",
-      expr = true,
-    },
   },
 }
