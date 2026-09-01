73eab6022b585964a48b82fc3312dc7819569a2b
Andres Gutierrez <agutierrez.codes@gmail.com>
2026-09-01 16:47:48 +0200

    refactor(blink(: remove lsp markdown oxide lsp provider

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -103,12 +103,12 @@ return {
         },
 
         providers = {
-          lsp = {
-            -- markdown_oxide wiki-link completion: include [ ] / # space in keyword
-            markdown_oxide = {
-              keyword_pattern = [[[\[\]\k\| \/\#]\+]],
-            },
-          },
+          -- lsp = {
+          --   -- markdown_oxide wiki-link completion: include [ ] / # space in keyword
+          --   markdown_oxide = {
+          --     keyword_pattern = [[[\[\]\k\| \/\#]\+]],
+          --   },
+          -- },
           snippets = {
             name = "snippets",
             module = "blink.cmp.sources.snippets",
