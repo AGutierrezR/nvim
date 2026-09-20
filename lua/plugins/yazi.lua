d81ba152b801c2b3aa3225a571e1e859f4958e75
Andres Gutierrez <agutierrez.codes@gmail.com>
2026-09-20 14:01:11 +0200

    feat(plugins): add yazi

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,22 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*", -- use the latest stable version
+  -- event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<C-w><C-e>",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+}
